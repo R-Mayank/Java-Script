a5794aaf1d1fbdfc266cfe7ed4d4317ad5d28b03
Mayank-Radadiya <juug22btech42412@gmail.com>
2024-04-08 21:25:46 +0530

    Create function.js

---
+++ JAVA Script Basic/function.js
@@ -0,0 +1,26 @@
+// function   
+
+function x(){
+   console.log("Hello World!!!"); 
+}
+
+//Now Function Call
+x();
+
+
+
+//Using Function on Given Input:
+
+
+//      function function_name(parameters){
+//         code;
+//    }
+//    function(arguments)
+
+function add(a,b){
+    c = a+b;
+    console.log(c);
+}
+
+add(5,10);
+
